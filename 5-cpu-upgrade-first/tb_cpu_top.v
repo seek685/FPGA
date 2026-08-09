664e25b77790ad0b2f70b6e49d5ee245eb7eba33
seek685 <1183069290@qq.com>
2026-08-09 21:59:27 +0800

    cpu first upgrade

---
+++ 5-cpu-upgrade-first/tb_cpu_top.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ns
+module tb_cpu_top;
+    reg clk;
+    reg rst_n;
+    initial clk=0;
+    always #5 clk=~clk;
+    cpu_top dut(
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+    initial begin
+        rst_n=0;
+        repeat(3) @(posedge clk);
+        #1 rst_n=1;
+        #200;
+    end
+endmodule
